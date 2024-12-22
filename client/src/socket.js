5d43560860d18ff46b5401cc0b06a99a0b9810fc
Nitesh <117444692+17Nitesh@users.noreply.github.com>
2024-12-22 22:17:51 +0530

    Update socket.js

--- client/src/socket.js
+++ client/src/socket.js
@@ -1,6 +1,6 @@
 import { io } from 'socket.io-client';
 
-const socket = io('https://sketch-scribe-server.onrender.com);  // Replace with your server URL
+const socket = io('https://sketch-scribe-server.onrender.com');  // Replace with your server URL
 
 socket.on('connect', () => {
     console.log('Connected to server with socket ID:', socket.id);  // This should log the socket ID
