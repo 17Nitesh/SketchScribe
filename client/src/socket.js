c03e367ce4c06ae674905bdf82698d3d66f8b2ac
Nitesh <117444692+17Nitesh@users.noreply.github.com>
2024-12-22 21:26:43 +0530

    Update socket.js

--- client/src/socket.js
+++ client/src/socket.js
@@ -1,4 +1,12 @@
 import { io } from 'socket.io-client';
 
-const socket = io('https://sketch-scribe-server-edvk1wr3d-niteshs-projects-08f90836.vercel.app/'); // Replace with your server URL
+const socket = io('https://sketch-scribe-server-jyc5g3f7g-niteshs-projects-08f90836.vercel.app');  // Replace with your server URL
+
+socket.on('connect', () => {
+    console.log('Connected to server with socket ID:', socket.id);  // This should log the socket ID
+});
+
+socket.on('disconnect', () => {
+    console.log('Disconnected from server');
+});
 export default socket;
